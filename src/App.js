f25d19b07b39dfddd78ed182aec17ac8b104c76b
Ishaan-11 <ishaansharma1998@gmail.com>
2021-01-06 21:28:57 +0530

    get images from api and display

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import PhotoContextProvider from "./context/PhotoContext";
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import Header from "./components/Header";
+import Item from "./components/Item";
 
 function App() {
 
@@ -12,18 +14,27 @@ function App() {
   }
 
   return (
-    <HashRouter basename="/SnapScout">
-      <div className="container">
-        <Route 
-          render={props => (
-            <Header 
-            handleSubmit={handleSubmit}
-              history={props.history}
-            />
-          )} 
-        />
-      </div>
-    </HashRouter>
+    <PhotoContextProvider>
+      <HashRouter basename="/SnapScout">
+        <div className="container">
+          <Route 
+            render={props => (
+              <Header 
+              handleSubmit={handleSubmit}
+                history={props.history}
+              />
+            )} 
+          />
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/mountain" />} />
+            <Route exact path="/mountain" render={() => <Item searchTerm="mountain" />} />
+            <Route exact path="/beach" render={() => <Item searchTerm="beach" />} />
+            <Route exact path="/bird" render={() => <Item searchTerm="bird" />} />
+            <Route exact path="/food" render={() => <Item searchTerm="food" />} />
+          </Switch>
+        </div>
+      </HashRouter>
+    </PhotoContextProvider>
   );
 }
 
